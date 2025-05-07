bfc150c88f8dcc621277b40965c8b4deaf135ecc
Copubah <copubah@gmail.com>
2025-05-07 11:15:35 +0300

    hotfix

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-1"
 }
 
+# Create the SNS topic
 resource "aws_sns_topic" "order_notifications" {
   name = "order-notifications-topic"
 }
 
+# Create the SQS queues
 resource "aws_sqs_queue" "inventory_queue" {
   name = "inventory-queue"
 }
@@ -18,6 +20,7 @@ resource "aws_sqs_queue" "analytics_queue" {
   name = "analytics-queue"
 }
 
+# Create SNS subscriptions for the SQS queues
 resource "aws_sns_topic_subscription" "inventory_subscription" {
   topic_arn = aws_sns_topic.order_notifications.arn
   protocol  = "sqs"
@@ -39,6 +42,7 @@ resource "aws_sns_topic_subscription" "analytics_subscription" {
   depends_on = [aws_sqs_queue.analytics_queue]
 }
 
+# IAM role for SNS to assume
 resource "aws_iam_role" "sns_sqs_role" {
   name = "sns-sqs-role"
   assume_role_policy = <<EOF
@@ -57,6 +61,7 @@ resource "aws_iam_role" "sns_sqs_role" {
 EOF
 }
 
+# IAM policy to allow SNS to send messages to the SQS queues
 resource "aws_iam_role_policy" "sns_sqs_policy" {
   role = aws_iam_role.sns_sqs_role.id
   policy = jsonencode({
@@ -65,7 +70,55 @@ resource "aws_iam_role_policy" "sns_sqs_policy" {
       {
         Effect   = "Allow",
         Action   = ["sqs:SendMessage"],
-        Resource = [aws_sqs_queue.inventory_queue.arn, aws_sqs_queue.shipping_queue.arn, aws_sqs_queue.analytics_queue.arn]
+        Resource = [
+          aws_sqs_queue.inventory_queue.arn,
+          aws_sqs_queue.shipping_queue.arn,
+          aws_sqs_queue.analytics_queue.arn
+        ]
+      }
+    ]
+  })
+}
+
+# SNS Topic Policy to allow SNS to send messages to the SQS queues
+resource "aws_sns_topic_policy" "sns_topic_policy" {
+  arn = aws_sns_topic.order_notifications.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "SNS:Publish",
+        Resource = aws_sns_topic.order_notifications.arn,
+        Principal = "*",
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sqs_queue.inventory_queue.arn
+          }
+        }
+      },
+      {
+        Effect   = "Allow",
+        Action   = "SNS:Publish",
+        Resource = aws_sns_topic.order_notifications.arn,
+        Principal = "*",
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sqs_queue.shipping_queue.arn
+          }
+        }
+      },
+      {
+        Effect   = "Allow",
+        Action   = "SNS:Publish",
+        Resource = aws_sns_topic.order_notifications.arn,
+        Principal = "*",
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sqs_queue.analytics_queue.arn
+          }
+        }
       }
     ]
   })
